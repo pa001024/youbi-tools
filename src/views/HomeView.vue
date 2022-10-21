be04df38851a30df48692f6d220339acf22e6076
pa001024 <pa001024@qq.com>
2022-10-22 02:51:08 +0800

    icon

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,14 @@
     <h1>幽闭星光</h1>
     <p>
       <a href="https://shimo.im/docs/NJkbEJM0RPFDVZqR/" target="_blank" rel="noopener noreferrer">《幽闭星光公告板/会议记录》</a>
+      <pre>
+        300指挥:
+高机动不增援队 https://youbi-tools.ml/cost/0t60OA1l50a515A1U520A1WA0ZA0HA
+增援5狩猎 https://youbi-tools.ml/cost/0t80OA1l81WA0ZA0HA1J10S61Q520A1581D2
+
+400指挥:
+后期大船队 增援5鱼 https://youbi-tools.ml/cost/1F70w81Q509A1OA1S51YA20A21A1ZA0QA1pA0ZA0HA
+      </pre>
     </p>
   </main>
 </template>
