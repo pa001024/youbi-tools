95eb28dabe826f726d4f2d9a4d67538e0cd4701e
pa001024 <pa001024@qq.com>
2022-11-02 16:05:26 +0800

    fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,11 +7,12 @@
       <a href="https://shimo.im/docs/NJkbEJM0RPFDVZqR/" target="_blank" rel="noopener noreferrer">《幽闭星光公告板/会议记录》</a>
       <pre>
         300指挥:
-高机动不增援队 https://youbi-tools.ml/cost/0t60OA1l50a515A1U520A1WA0ZA0HA
-增援5狩猎 https://youbi-tools.ml/cost/0t80OA1l81WA0ZA0HA1J10S61Q520A1581D2
+高机动不增援队 <a href="/cost/0t60OA1l50a515A1U520A1WA0ZA0HA">配置</a>
+增援5狩猎 <a href="/cost/0t80OA1l81WA0ZA0HA1J10S61Q520A1581D2">配置</a>
 
 400指挥:
-后期大船队 增援5鱼 https://youbi-tools.ml/cost/1F70w81Q509A1OA1S51YA20A21A1ZA0QA1pA0ZA0HA
+11CV高速航斩 (推荐) <a href="/cost/1151pA24A0w80OA0n520A1J11a11w10I31YA0HF0Z91DA">配置</a>
+后期大船队 增援5鱼 <a href="/cost/1F70w81Q509A1OA1S51YA20A21A1ZA0QA1pA0ZA0HA">配置</a>
       </pre>
     </p>
   </main>
